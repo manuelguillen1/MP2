8bf596d3edad71baf70a7ea3b1effe529b064c91
Manuel Guillen <manuelguillen1@yahoo.com>
2024-01-24 17:05:24 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+seed = [0]
+
+@app.route('/', methods = ["GET"])
+def get_request():
+    return str(seed[0])
+
+@app.route('/', methods = ["POST"])
+def post_request():
+    set_seed(seed, request.get_json()["num"])
+    return str(seed[0])
+
+def set_seed(seed, num):
+    seed[0] = num
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', debug = True, port = 8080)
---
+++ serve.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+import socket
+import subprocess
+app = Flask(__name__)
+
+@app.route('/', methods = ["GET", "POST"])
+def handler():
+    if request.method == "POST":
+        subprocess.Popen(["python", "stress_cpu.py"])
+    return socket.gethostname()
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", debug = True, port = 8080)
