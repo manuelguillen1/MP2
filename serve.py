6a2bd79c5b69b69959e364dfa3766bf6170f97be
Manuel Guillen <manuelguillen1@yahoo.com>
2024-01-24 21:31:48 -0800

    Changed port back to 8080 and updated POST return

--- serve.py
+++ serve.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 def handler():
     if request.method == "POST":
         subprocess.Popen(["python", "stress_cpu.py"])
-    return socket.gethostname()
+        return "POST REQUEST SUCCEEDED"
+    else:
+        return socket.gethostname()
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", debug = True, port = 8081)
+    app.run(host = "0.0.0.0", debug = True, port = 8080)
